5d2a5149f555bbe60314d9ced69e6063657b364e
Kim <kimtuan.doan@gmail.com>
2022-01-13 00:18:30 +0100

    country summary added. APIV2.

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { createElement, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Line } from "react-chartjs-2";
 import { COLORS } from "../assets/data/colors";
 import { countriesList } from "../assets/data/countries";
@@ -10,13 +10,19 @@ function Country() {
     const [days, setDays] = useState("30");
     const [apiresult, setApiResult] = useState("");
     const [data, setData] = useState({ labels: ["J", "F"], datasets: [{ label: "a", backgroundColor: COLORS.yellow.border, borderColor: COLORS.yellow.border, data: [1, 2] }] });
-    const [filter, setFilter] = useState(3);
+    const [scale, setScale] = useState(1);
+    // const [ratio, setRatio] = useState({maintainAspectRatio: true});
+
 
     const refLineChart0 = useRef(null);
     const refLineChart1 = useRef(null);
     const refLineChart2 = useRef(null);
 
-    async function fetchData(_country, _days) {
+    function fetchData(c, d) {
+        fetchDataAPI2(c, d);
+    }
+
+    async function fetchDataAPI1(_country, _days) {
         const _url0 = `https://api.covid19api.com/total/dayone/country/${_country}`;
         const _url = `https://corona.lmao.ninja/v2/historical/${_country}?lastdays=${_days}`;
 
@@ -78,6 +84,88 @@ function Country() {
         }
     }
 
+    async function fetchDataAPI2(_country, _days) {
+
+        const _url1 = `https://disease.sh/v3/covid-19/countries/${_country}`;
+        const _url2 = `https://disease.sh/v3/covid-19/historical/${_country}?lastdays=${_days}`;
+
+        try {
+            const response = await axios.get(_url1);
+            const d = response.data;
+            const _container = document.querySelector(".country-summary");
+            _container.innerHTML = `
+            <div><img src="${d.countryInfo.flag}" alt="${d.country}"></img></div>
+            <div class="summary-case"><span>Population</span><span>${d.population.toLocaleString()}</span></div>
+            <div class="summary-case"><span>active</span><span>${d.active.toLocaleString()}</span></div>
+            <div class="summary-case"><span>new cases</span><span>${d.todayCases.toLocaleString()}</span></div>
+            <div class="summary-case"><span>new Deaths</span><span>${d.todayDeaths.toLocaleString()}</span></div>
+            <div class="summary-case"><span>new Recovered</span><span>${d.todayRecovered.toLocaleString()}</span></div>
+            <div class="summary-case"><span>cases</span><span>${d.cases.toLocaleString()}</span></div>
+            <div class="summary-case"><span>deaths</span><span>${d.deaths.toLocaleString()}</span></div>
+            <div class="summary-case"><span>recovered</span><span>${d.recovered.toLocaleString()}</span></div>
+            <div class="summary-case"><span>tests</span><span>${d.tests.toLocaleString()}</span></div>
+            <div class="summary-case"><span>lethality rate</span><span>${(d.deaths/d.cases).toFixed(3)+"%"}</span></div>
+            `;
+
+        } catch (e) {
+            // console.log(e);
+            setApiResult(() => _country + ": country not found");
+        }
+
+        try {
+            const response = await axios.get(_url2);
+            setCountry(_country);
+            document.querySelector("form").reset();
+            setDays(() => _days);
+            const labelArray = Object.keys(response.data.timeline.cases);
+            const caseArray = Object.values(response.data.timeline.cases);
+            const deathArray = Object.values(response.data.timeline.deaths);
+
+            const _newLineData = {
+                labels: labelArray, datasets: [
+                    { label: "Total Cases", backgroundColor: COLORS.yellow.border, borderColor: COLORS.yellow.border, data: caseArray }
+                ]
+            };
+            const _newLineData2 = {
+                labels: labelArray, datasets: [
+                    { label: "Total Deaths", backgroundColor: COLORS.blue.border, borderColor: COLORS.blue.border, data: deathArray }
+                ]
+            };
+
+            const caseIncrementArray = [];
+            const deathIncrementArray = [];
+
+            for (let i = 0; i < caseArray.length; i++) {
+                if (i === 0) {
+                    caseIncrementArray.push(0);
+                    deathIncrementArray.push(0);
+                } else {
+                    caseIncrementArray.push(caseArray[i] - caseArray[i - 1]);
+                    deathIncrementArray.push(deathArray[i] - deathArray[i - 1]);
+                }
+            }
+
+            const start = labelArray.length - _days;
+            const end = labelArray.length + 1;
+            const _newLineData0 = {
+                labels: labelArray.slice(start, end), datasets: [
+                    { label: "New Cases", backgroundColor: COLORS.green.border, borderColor: COLORS.green.border, data: caseIncrementArray.slice(start, end) },
+                    { label: "New Deaths", backgroundColor: COLORS.gray.border, borderColor: COLORS.gray.border, data: deathIncrementArray.slice(start, end) }
+                ]
+            };
+
+            refLineChart0.current.data = _newLineData0;
+            refLineChart1.current.data = _newLineData;
+            refLineChart2.current.data = _newLineData2;
+            refLineChart0.current.update();
+            refLineChart1.current.update();
+            refLineChart2.current.update();
+        } catch (e) {
+            // console.log(e);
+            setApiResult(() => _country + ": country not found");
+        }
+    }
+
     useEffect(() => {
         fetchData("France", "30");
     }, [])
@@ -124,6 +212,16 @@ function Country() {
         fetchData(this.value, "30");
     }
 
+    // function changeScale(x) {
+    //     setScale(() => x);
+    //     let bool=false;
+
+    //     if( x === 1 ) {
+    //         bool = true;
+    //     }
+    //     setRatio(() => {return { maintainAspectRatio: bool}} );
+    // }
+
     return <>
         <section className="search-container">
             <form method="post" onSubmit={changeCountry} onReset={resetSuggestions}>
@@ -133,13 +231,35 @@ function Country() {
             <span></span>
         </section>
         <h2>{country}</h2>
+
+        <section className="country-summary">
+            <div><img src="https://disease.sh/assets/img/flags/fr.png" alt="france"></img></div>
+            <div className="summary-case"><span>Population</span><span>65494103</span></div>
+            <div className="summary-case"><span>active</span><span>4045100</span></div>
+            <div className="summary-case"><span>todayCases</span><span>361719</span></div>
+            <div className="summary-case"><span>todayDeaths</span><span>246</span></div>
+            <div className="summary-case"><span>todayRecovered</span><span>246</span></div>
+            <div className="summary-case"><span>cases</span><span>12934982</span></div>
+            <div className="summary-case"><span>deaths</span><span>126305</span></div>
+            <div className="summary-case"><span>recovered</span><span>126305</span></div>
+            <div className="summary-case"><span>tests</span><span>126305</span></div>
+            <div className="summary-case"><span>lethality rate</span><span>126305</span></div>
+
+        </section>
+
         <ul className="country-filters">
-            <li className={days === "all" ? "li-selected" : ""} onClick={() => { changeDays("all"); setFilter(() => 1) }}>all</li>
-            <li className={days === "90" ? "li-selected" : ""} onClick={() => { changeDays("90"); setFilter(() => 2) }}>last 3 months</li>
-            <li className={days === "30" ? "li-selected" : ""} onClick={() => { changeDays("30"); setFilter(() => 3) }}>last 30 days</li>
+            <div>
+                {/* <li className={scale === 1 ? "li-selected" : ""} onClick={() => changeScale(1)}>1x</li>
+                <li className={scale === 2 ? "li-selected" : ""} onClick={() => changeScale(2)}>2x</li> */}
+            </div>
+            <div>
+                <li className={days === "all" ? "li-selected" : ""} onClick={() => { changeDays("all") }}>all</li>
+                <li className={days === "90" ? "li-selected" : ""} onClick={() => { changeDays("90") }}>last 3 months</li>
+                <li className={days === "30" ? "li-selected" : ""} onClick={() => { changeDays("30") }}>last 30 days</li>
+            </div>
         </ul>
         <div className="country-chart-container">
-            <Line data={data} ref={refLineChart0} />
+            <div><div className={scale === 2 ? "scale2" : ""}><Line data={data} ref={refLineChart0} /></div></div>
             <Line data={data} ref={refLineChart1} />
             <Line data={data} ref={refLineChart2} />
         </div>
